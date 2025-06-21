1dbdff1034676c9289fe725f85e9519e16d7e751
Oleh Butenko <butenkoleh@gmail.com>
2025-06-21 23:25:55 +0300

    7 hw done 1 try

--- js/task-1.js
+++ js/task-1.js
@@ -1,31 +1,12 @@
-const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    // Change code below this line
-    getBalance() {
-      return this.balance;
-    },
-    getDiscount() {
-      return this.discount;
-    },
-    setDiscount(value) {
-        this.discount = value;
-    },
-    getOrders() {
-      return this.orders;
-    },
-    addOrder(cost, order) {
-        this.balance -= cost - cost * this.discount;
-        this.orders.push(order);
-    },
-    // Change code above this line
-  };
-  
-  customer.setDiscount(0.15);
-  console.log(customer.getDiscount()); // 0.15
-  customer.addOrder(5000, "Steak");
-  console.log(customer.getBalance()); // 19750
-  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-  
+const categoriesItems = document.querySelectorAll("#categories .item");
+
+const message = `Number of categories: ${categoriesItems.length}`
+
+console.log(message);
+
+categoriesItems.forEach(item => {
+    const title = item.querySelector("h2").textContent;
+    const items = item.querySelectorAll("li");
+    console.log(`Categorie: ${title}`);
+    console.log(`Elements: ${items.length}`);
+})
---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const elements = event.target.elements;
+    if (elements.email.value === "" || elements.password.value === "") {
+        alert('All form fields must be filled in')
+    } else {
+        const info = {
+            email: elements.email.value.trim(),
+            password: elements.password.value.trim()
+        }
+        console.log(info);
+        form.reset();
+    }
+    
+})
